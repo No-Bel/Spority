3707315cd21b964ce409e01f7294b43dd436ca7d
Nodar Belashvili <nodarbelashvili@gmail.com>
2024-05-21 14:11:39 +0400

    crate SongViewModel

---
+++ app/src/main/java/com/example/spotify/ui/viewmodel/SongViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.spotify.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.spotify.exoplayer.MusicService
+import com.example.spotify.exoplayer.MusicServiceConnection
+import com.example.spotify.util.Constants.UPDATE_PLAYER_POSITION_INTERVAL
+import com.example.spotify.util.extensions.currentPlayBackPosition
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SongViewModel @Inject constructor(
+    musicServiceConnection: MusicServiceConnection
+) : ViewModel() {
+
+    private val playbackState = musicServiceConnection.playbackState
+
+    private val _curSongDuration = MutableLiveData<Long>()
+    val curSongDuration: LiveData<Long> = _curSongDuration
+
+    private val _curPlayerPosition = MutableLiveData<Long>()
+    val curPlayerPosition: LiveData<Long> = _curPlayerPosition
+
+    init {
+        updateCurrentPlayerPosition()
+    }
+
+    private fun updateCurrentPlayerPosition() {
+        viewModelScope.launch {
+            while (true) {
+                val pos = playbackState.value?.currentPlayBackPosition
+                if (curPlayerPosition.value != pos) {
+                    _curPlayerPosition.postValue(pos!!)
+                    _curSongDuration.postValue(MusicService.curSongDuration)
+                }
+                delay(UPDATE_PLAYER_POSITION_INTERVAL)
+            }
+        }
+    }
+}
