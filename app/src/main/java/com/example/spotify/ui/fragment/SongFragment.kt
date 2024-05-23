2ca4c2c5a4f989db844e1bdea4dd2b4e7a43932d
Nodar Belashvili <nodarbelashvili@gmail.com>
2024-05-23 14:34:05 +0400

    setting up the SeekBar

--- app/src/main/java/com/example/spotify/ui/fragment/SongFragment.kt
+++ app/src/main/java/com/example/spotify/ui/fragment/SongFragment.kt
@@ -1,10 +1,13 @@
 package com.example.spotify.ui.fragment
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.RequestManager
@@ -14,8 +17,11 @@ import com.example.spotify.databinding.FragmentSongBinding
 import com.example.spotify.ui.viewmodel.MainViewModel
 import com.example.spotify.ui.viewmodel.SongViewModel
 import com.example.spotify.util.Status
+import com.example.spotify.util.extensions.isPlaying
 import com.example.spotify.util.extensions.toSong
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.Locale
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -31,6 +37,10 @@ class SongFragment : Fragment() {
 
     private var curPlayingSong: Song? = null
 
+    private var playbackState: PlaybackStateCompat? = null
+
+    private var shouldUpdateSeekbar = true
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,6 +54,42 @@ class SongFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
         subscribeToObservers()
+        listeners()
+    }
+
+    private fun listeners() {
+        binding.ivPlayPauseDetail.setOnClickListener {
+            curPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it, true)
+            }
+        }
+
+        binding.ivSkipPrevious.setOnClickListener {
+            mainViewModel.skipToPreviousSong()
+        }
+
+        binding.ivSkip.setOnClickListener {
+            mainViewModel.skipToNextSong()
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    setCurPlayerTimeToTextView(progress.toLong())
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                shouldUpdateSeekbar = false
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                binding.seekBar.let {
+                    mainViewModel.seekTo(it.progress.toLong())
+                    shouldUpdateSeekbar = true
+                }
+            }
+        })
     }
 
     private fun updateTitleAndSongImage(song: Song) {
@@ -74,6 +120,32 @@ class SongFragment : Fragment() {
             curPlayingSong = it.toSong()
             updateTitleAndSongImage(curPlayingSong!!)
         }
+
+        mainViewModel.playbackState.observe(viewLifecycleOwner) {
+            playbackState = it
+            binding.ivPlayPauseDetail.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+            binding.seekBar.progress = it?.position?.toInt() ?: 0
+        }
+
+        songViewModel.curPlayerPosition.observe(viewLifecycleOwner) {
+            if (shouldUpdateSeekbar) {
+                binding.seekBar.progress = it.toInt()
+                setCurPlayerTimeToTextView(it)
+            }
+        }
+
+        songViewModel.curSongDuration.observe(viewLifecycleOwner) {
+            binding.seekBar.max = it.toInt()
+            val dataFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+            binding.tvSongDuration.text = dataFormat.format(it)
+        }
+    }
+
+    private fun setCurPlayerTimeToTextView(ms: Long?) {
+        val dataFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+        binding.tvCurTime.text = dataFormat.format(ms)
     }
 
     override fun onDestroy() {
